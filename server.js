549da693c7f619cbe282ed7954c01b3713c83953
Jonathan Graziani <jpgraziani@gmail.com>
2021-01-02 20:52:26 -0700

    updated validation with parms for search

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
-const express = require('express');
-const morgan = require('morgan');
-const POKEDEX = require('./pokedex.json');
+const express = require('express')
+const morgan = require('morgan')
+const POKEDEX = require('./pokedex.json')
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-
-//hardcode to send it back as json as test
-const validTypes = ['Bug', 'Dark', `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+app.use(morgan('dev'))
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -21,24 +18,35 @@ app.use(function validateBearerToken(req, res, next) {
   next()
 })
 
-//2. use seperate function to handle the request
-function handleGetTypes(req, res) {
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.get('/types', function handleGetTypes(req, res) {
   res.json(validTypes)
-}
+})
 
-//1. use app.get to construct our endpoint
-//3. for End point /types is first arg handleGetTypes is second type. second is a callback func. This done to create reusability
-app.get('/types', handleGetTypes)
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
 
-//these handles are middleware and should happend to validate the request before the app.get
-function handleGetPokemon(req, res) {
-  res.send("hello Pokemon")
-}
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
 
-app.get('/pokemon', handleGetPokemon)
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
 
-const PORT = 8000;
+  res.json(response)
+})
+
+const PORT = 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
